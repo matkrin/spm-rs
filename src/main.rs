fb26259d00f3f9ec48eeb33936ada0a85dd847e9
matkrin <matkrin@protonmail.com>
2022-10-20 07:01:46 +0200

    Added clap

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
+use clap::Parser;
 use spm_rs::mulfile::read_mul;
 use spm_rs::omicron_matrix::read_omicron_matrix;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about=None)]
+struct Args {
+    #[arg()]
+    filename: String,
+}
+
 fn main() {
-    let mulfile = read_mul("tests/test_files/stm-aarhus-mul-a.mul");
-
-    for mut i in mulfile {
-        i.img_data.correct_plane();
-        i.img_data.correct_lines();
-        i.img_data.save_png();
+    let args = Args::parse();
+    if args.filename.ends_with(".mul") || args.filename.ends_with(".flm") {
+        let mulfile = read_mul(&args.filename);
+        for mut i in mulfile {
+            i.img_data.correct_plane();
+            i.img_data.correct_lines();
+            i.img_data.save_png();
+        }
+    } else if args.filename.ends_with(".Z_mtrx") {
+        let mut omicron_matrix = read_omicron_matrix(&args.filename);
+        omicron_matrix.img_data_fw.correct_plane();
+        omicron_matrix.img_data_bw.correct_lines();
+        omicron_matrix.img_data_fw.save_png();
+        // omicron_matrix.img_data_bw.save_png();
     }
-
-    let mut omicron_matrix = read_omicron_matrix("tests/test_files/20201111--4_1.Z_mtrx");
-    println!("current: {}", omicron_matrix.current);
-    println!("bias: {}", omicron_matrix.bias);
-    println!("xsize: {}", omicron_matrix.xsize);
-    println!("ysize: {}", omicron_matrix.ysize);
-    println!("xres: {}", omicron_matrix.xres);
-    println!("yres: {}", omicron_matrix.yres);
-    println!("rotation: {}", omicron_matrix.rotation);
-    omicron_matrix.img_data_fw.save_png();
-    omicron_matrix.img_data_bw.save_png();
-    // omicron_matrix.correct_plane();
-    // omicron_matrix.correct_lines();
-    // omicron_matrix.save_png();
 }
